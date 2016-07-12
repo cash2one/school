ae7301108b97806615945f0f78b62aa9c4fed74e
dyyhobby <dyy@dyy.name>
2016-07-12 16:32:11 +0800

    更新

--- app/Http/Middleware/HasFamily.php
+++ app/Http/Middleware/HasFamily.php
@@ -3,19 +3,18 @@
 namespace App\Http\Middleware;
 
 use Closure;
-
+use Illuminate\Support\Facades\Auth;
 class HasFamily
 {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param $request
+     * @param Closure $next
+     * @param null $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next,$guard = null)
     {
-        if(!$request->user()->hasRole('family'))
+        if(Auth::guard($guard)->guest() && !$request->user()->hasRole('family'))
         {
             return redirect('/student/bind');
         }
