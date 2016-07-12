44987752a5b067306fb0a11ed396d6c7d34abff6
dyyhobby <dyy@dyy.name>
2016-07-12 10:40:04 +0800

    更新

--- config/wechat.php
+++ config/wechat.php
@@ -16,10 +16,10 @@ return [
     /**
      * 账号基本信息，请从微信公众平台/开放平台获取
      */
-    'app_id'  => env('WECHAT_APPID', 'your-app-id'),         // AppID
-    'secret'  => env('WECHAT_SECRET', 'your-app-secret'),     // AppSecret
-    'token'   => env('WECHAT_TOKEN', 'your-token'),          // Token
-    'aes_key' => env('WECHAT_AES_KEY', ''),                    // EncodingAESKey
+    'app_id'  => 'wxc3b49aefbaa030fa',         // AppID
+    'secret'  => 'b8047b9919212ddc6067ecf5c6a78017',     // AppSecret
+    'token'   => 'b8047b9919212ddc6067ecf5c6a78017',          // Token
+    'aes_key' => 'b8047b9919212ddc6067ecf5c6a78017',                    // EncodingAESKey
 
     /**
      * 日志配置
@@ -29,8 +29,8 @@ return [
      * file：日志文件位置(绝对路径!!!)，要求可写权限
      */
     'log' => [
-        'level' => env('WECHAT_LOG_LEVEL', 'debug'),
-        'file'  => env('WECHAT_LOG_FILE', storage_path('logs/wechat.log')),
+        'level' => 'debug',
+        'file'  => storage_path('logs/wechat.log'),
     ],
 
     /**
@@ -39,22 +39,22 @@ return [
      * scopes：公众平台（snsapi_userinfo / snsapi_base），开放平台：snsapi_login
      * callback：OAuth授权完成后的回调页地址(如果使用中间件，则随便填写。。。)
      */
-    // 'oauth' => [
-    //     'scopes'   => array_map('trim', explode(',', env('WECHAT_OAUTH_SCOPES', 'snsapi_userinfo'))),
-    //     'callback' => env('WECHAT_OAUTH_CALLBACK', '/examples/oauth_callback.php'),
-    // ],
+     'oauth' => [
+         'scopes'   => 'snsapi_userinfo',
+         'callback' => env('WECHAT_OAUTH_CALLBACK', '/examples/oauth_callback.php'),
+     ],
 
     /**
      * 微信支付
      */
-    // 'payment' => [
-    //     'merchant_id'        => env('WECHAT_PAYMENT_MERCHANT_ID', 'your-mch-id'),
-    //     'key'                => env('WECHAT_PAYMENT_KEY', 'key-for-signature'),
-    //     'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/your/cert.pem'), // XXX: 绝对路径！！！！
-    //     'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'path/to/your/key'),      // XXX: 绝对路径！！！！
-    //     // 'device_info'     => env('WECHAT_PAYMENT_DEVICE_INFO', ''),
-    //     // 'sub_app_id'      => env('WECHAT_PAYMENT_SUB_APP_ID', ''),
+     'payment' => [
+         'merchant_id'        => '1335646501',
+         'key'                => 'b8047b9919212ddc6067ecf5c6a78017',
+         'cert_path'          => env('WECHAT_PAYMENT_CERT_PATH', 'path/to/your/cert.pem'), // XXX: 绝对路径！！！！
+         'key_path'           => env('WECHAT_PAYMENT_KEY_PATH', 'path/to/your/key'),      // XXX: 绝对路径！！！！
+         // 'device_info'     => env('WECHAT_PAYMENT_DEVICE_INFO', ''),
+         // 'sub_app_id'      => env('WECHAT_PAYMENT_SUB_APP_ID', ''),
     //     // 'sub_merchant_id' => env('WECHAT_PAYMENT_SUB_MERCHANT_ID', ''),
     //     // ...
-    // ],
+     ],
 ];
