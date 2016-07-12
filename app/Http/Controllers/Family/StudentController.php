5ee2821819d7970c054216049452b715aad77819
dyyhobby <dyy@dyy.name>
2016-07-12 20:55:11 +0800

    更新

--- app/Http/Controllers/Family/StudentController.php
+++ app/Http/Controllers/Family/StudentController.php
@@ -92,7 +92,7 @@ class StudentController extends FamilyController
 
             $totals[$i] = $item->scores->where('student_id',$student->id)->sum('val');
 
-            dd($item->scores->where('student_id',$student->id)->toSql());
+            $i++;
         }
 
         return view('family.student.detail',[
