4833f636b4dadacd62e91cd1573464d5965edd7a
dyyhobby <dyy@dyy.name>
2016-07-13 16:00:51 +0800

    更新

--- app/Jobs/CreateActivityScore.php
+++ app/Jobs/CreateActivityScore.php
@@ -36,7 +36,7 @@ class CreateActivityScore extends Job implements ShouldQueue
      */
     public function handle()
     {
-        $students = $this->classes->students;
+        $students = $this->classes;
 
         dd($students);
 
