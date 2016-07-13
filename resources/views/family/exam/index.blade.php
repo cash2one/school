277c528b98d0d9bca709f75d74c7360a99425608
dyyhobby <dyy@dyy.name>
2016-07-13 10:30:13 +0800

    更新

--- resources/views/family/exam/index.blade.php
+++ resources/views/family/exam/index.blade.php
@@ -1,7 +1,36 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dyyho
- * Date: 2016/7/13
- * Time: 10:21
- */
+<!DOCTYPE html >
+<html lang="zh-cmn-Hans">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <meta name="description" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no">
+    <title>全部考试</title>
+    <link rel="stylesheet" type="text/css" href="/css/common.css" >
+    <link rel="stylesheet" type="text/css" href="/css/style.css" >
+</head>
+<body>
+<div class="warp_bg">
+    <div class="notice_type">
+        <ul>
+            <li><a href="{{ url('/family/task/student',['id' => $student->id]) }}">作业</a></li>
+            <li class="cur"><a href="{{ url('/family/activity/student',['id' => $student->id]) }}">活动</a></li>
+        </ul>
+    </div>
+    <div class="notice">
+        <ul>
+            @foreach($exams as $exam)
+                <li>
+                    <a href="{{ url('/family/exam/detail',['id' => $exam->id]) }}">
+                        <div class="notice_cont">
+                            <h3>{{ $exam->name }}</h3>
+                            <p><span class="fl">{{ $exam->created_at }}</span>{{ $exam->grade->name }} {{ $exam->classes->name }}</p>
+                        </div>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
+</body>
+</html>
