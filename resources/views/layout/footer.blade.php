96dea4574dc9a824a5826f0ee3b15727b24c40b8
dyyhobby <dyy@dyy.name>
2016-07-14 10:58:31 +0800

    更新

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -35,11 +35,18 @@
         </li>
     </ul>
 </div>
+@if(Auth::user()->hasRole('teacher') && Auth::user()->hasOne('parents'))
+<div class="fix_btn">
+    <a href="{{ url('/') }}">身份选择</a>
+</div>
+@endif
 <style>
     .bot_nav{ height: 4rem; position: fixed; left: 0; bottom: 0; width: 100%; border-top: .1rem solid #ddd; background: #fff; padding: 1rem 0 .5rem;}
     .bot_nav li{ width: 25%; text-align: center;float: left;}
     .bot_nav i{ display: inline-block; width: 2rem; height: 2rem;}
     .bot_nav p{ line-height: 1.6rem; height: 1.6rem;  font-size: 1.4rem; }
+    .fix_btn{ position: fixed; bottom: 10rem; right: 0; border-top-left-radius: .5rem;
+        border-bottom-left-radius: .5rem; background: rgba(0,172,146,0.7); border: .1rem solid #00AC92; width: 2rem; text-align: center; padding: 1rem .5rem; color: #fff;}
 </style>
 <script language="javascript" type="text/javascript" src="/js/jquery.js"></script>
 <script language="javascript" type="text/javascript" src="/js/jquery.noty.packaged.min.js"></script>
