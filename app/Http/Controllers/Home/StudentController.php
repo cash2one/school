6a97a621d954b03c8721bf9336abe58f3cda078f
dyyhobby <dyy@dyy.name>
2016-07-28 16:07:24 +0800

    更新

--- app/Http/Controllers/Home/StudentController.php
+++ app/Http/Controllers/Home/StudentController.php
@@ -101,7 +101,7 @@ class StudentController extends HomeController
 
         $moblieSms = DB::table('laravel_sms')->where('to',$request->family_mobile)->orderBy('id','desc')->first();
 
-        if((time() - $moblieSms->sent_time) > 300)
+        if((time() - $moblieSms->sent_time) > 3600)
         {
             return redirect()->back()->with('status',[
                 'code' => 'error',
