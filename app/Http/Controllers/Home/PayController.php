4f4166b690d9884332c328fe5cc06cf0f82a7191
dyyhobby <dyy@dyy.name>
2016-08-01 15:54:19 +0800

    更新

--- app/Http/Controllers/Home/PayController.php
+++ app/Http/Controllers/Home/PayController.php
@@ -145,7 +145,7 @@ class PayController extends BaseController
 
         $status = $app->payment->query($order->number);
 
-        dd($status);
+        dd($status->return_code);
 
         return view('home.pay.status',[
             'status' => $status
