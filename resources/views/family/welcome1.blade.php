a13e0c95c14c39b01950729102cda1a5017103b0
dyyhobby <dyy@dyy.name>
2016-08-01 16:16:39 +0800

    更新

--- resources/views/family/welcome1.blade.php
+++ resources/views/family/welcome1.blade.php
@@ -13,8 +13,8 @@
 <div class="warp_bg">
     <div class="user_top">
         <div class="user_img"><img src="/images/face.png" /></div>
-        <p>学生姓名：蒋小花</p>
-        <p>班级：高一（5）班</p>
+        <p>学生姓名：{{ $student->name }}</p>
+        <p>班级：{{ $student->grade->name }}{{ $student->classes->name }}</p>
     </div>
     <div class="module">
         <ul>
@@ -25,19 +25,16 @@
                 <a href="{{ url('/family/message') }}"><span><img src="/images/icon/leave_user.png" /></span><i></i>我的留言</a>
             </li>
             <li>
-                <a href="../task_subject.html"><span><img src="/images/icon/icon_subject.png" /></span><i></i>作业查询</a>
+                <a href="{{ url('family/task/student',['id' => $student->id]) }}"><span><img src="/images/icon/icon_subject.png" /></span><i></i>作业查询</a>
             </li>
             <li>
-                <a href="grade.html"><span><img src="/images/icon/icon_grade.png" /></span><i></i>成绩查询</a>
+                <a href="{{ url('family/exam/student',['id' => $student->id]) }}"><span><img src="/images/icon/icon_grade.png" /></span><i></i>成绩查询</a>
             </li>
             <li>
-                <a href="habit_more.html"><span><img src="/images/icon/icon_habit_user.png" /></span><i></i>习惯养成</a>
+                <a href="{{ url('family/activity/student',['id' => $student->id]) }}"><span><img src="/images/icon/icon_habit_user.png" /></span><i></i>习惯养成</a>
             </li>
             <li>
-                <a href="teacher_list.html"><span><img src="/images/icon/icon_teacher.png" /></span><i></i>给老师留言</a>
-            </li>
-            <li>
-                <a href="children.html"><span><img src="/images/icon/icon_set.png" /></span><i></i><b>5</b>账号管理</a>
+                <a href="{{ url('/family/message/add',['sid' => $student->id]) }}"><span><img src="/images/icon/icon_teacher.png" /></span><i></i>给老师留言</a>
             </li>
         </ul>
     </div>
