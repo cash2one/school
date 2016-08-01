4923d0284b923f2d58114267be04af88a0db0d12
dyyhobby <dyy@dyy.name>
2016-08-01 16:00:42 +0800

    更新

--- resources/views/home/pay/status.blade.php
+++ resources/views/home/pay/status.blade.php
@@ -16,9 +16,14 @@
     .opera_link a{ display: block;  width: 10rem; margin: 1rem auto; height: 4rem; line-height: 4rem; color: #fff; background: #11CD6E; border-radius: .3rem; -webkit-border-radius: .3rem; -moz-border-radius: .3rem; text-align: center;}
 </style>
 <div class="opera">
-    <div class="opera_img"><img src="/images/icon/icon_success.png" /></div>
-    <p class="opera_text">缴费成功！</p>
-    <p class="opera_link"><a href="#">回首页</a></p>
+    @if($status->trade_state == 'SUCCESS')
+        <div class="opera_img"><img src="/images/icon/icon_success.png" /></div>
+        <p class="opera_text">支付成功！</p>
+    @else
+        <div class="opera_img"><img src="/images/icon/icon_fale.png" /></div>
+        <p class="opera_text">支付失败！</p>
+    @endif
+    <p class="opera_link"><a href="{{ url('/family') }}">返回</a></p>
 </div>
 <script language="javascript" type="text/javascript" src="/js/jquery.js"></script>
 </body>
