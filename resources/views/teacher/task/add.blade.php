1de684cabeedd1d18706332033be29542bf8d45f
dyyhobby <dyy@dyy.name>
2016-08-03 16:41:27 +0800

    更新

--- resources/views/teacher/task/add.blade.php
+++ resources/views/teacher/task/add.blade.php
@@ -39,7 +39,7 @@
 
                 if($(this).hasClass('now'))
                 {
-                    var input = $("<input name='course_id[]'>");
+                    var input = $("<input name='course_id[]' type='hidden'>");
 
                     input.val($(this).attr('data-id'));
 
