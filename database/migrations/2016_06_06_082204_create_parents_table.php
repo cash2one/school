7b29f6b28dfe4f1a6bd8dca798f30a5767fdcb3a
dyyhobby <dyy@dyy.name>
2016-08-03 14:16:53 +0800

    更新

--- database/migrations/2016_06_06_082204_create_parents_table.php
+++ database/migrations/2016_06_06_082204_create_parents_table.php
@@ -14,7 +14,9 @@ class CreateParentsTable extends Migration
     {
         Schema::create('parents', function (Blueprint $table) {
             $table->increments('id')->comment('自增长id');
+            $table->integer('user_id')->unsigned()->comment('用户id');
             $table->string('name')->comment('家长姓名');
+            $table->integer('auth_time')->comment('认证时间');
             $table->integer('created_at')->comment('创建时间');
             $table->integer('updated_at')->comment('更新时间');
             $table->integer('deleted_at')->nullable()->comment('删除时间');
