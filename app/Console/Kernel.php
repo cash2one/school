4a3158da2925db91701b8ea9a0ddafce21cba8e4
dyyhobby <dyy@dyy.name>
2016-08-04 15:04:10 +0800

    更新

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -34,21 +34,6 @@ class Kernel extends ConsoleKernel
 
             Log::info('来水计划任务');
 
-            $activity = new Activity();
-
-            $activitys = $activity->where('start_at','<=',time())->where('end_at','>=',time())->get();
-
-            $i = 1;
-
-            foreach ($activitys as $activity)
-            {
-                $createdActivityScore = (new CreateActivityScore($activity))->delay($i);
-
-                $this->dispatch($createdActivityScore);
-
-                $i++;
-            }
-
-        })->dailyAt('14:22');
+        })->everyMinute();
     }
 }
