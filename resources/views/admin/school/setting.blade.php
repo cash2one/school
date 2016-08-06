6e7df915a70ff2dfd946e7c516df8b955b50f37c
dyyhobby <dyy@dyy.name>
2016-08-06 16:39:48 +0800

    更新

--- resources/views/admin/school/setting.blade.php
+++ resources/views/admin/school/setting.blade.php
@@ -4,31 +4,7 @@
     <link href="/css/date/mobiscroll_002.css" rel="stylesheet" type="text/css">
     <link href="/css/date/mobiscroll.css" rel="stylesheet" type="text/css">
     <link href="/css/date/mobiscroll_003.css" rel="stylesheet" type="text/css">
-    <script src="/js/date/mobiscroll_002.js" type="text/javascript"></script>
-    <script src="/js/date/mobiscroll_004.js" type="text/javascript"></script>
-    <script src="/js/date/mobiscroll.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        $(function () {
-            var currYear = (new Date()).getFullYear();
-            var opt={};
-            opt.date = {preset : 'date'};
-            opt.datetime = {preset : 'datetime'};
-            opt.time = {preset : 'time'};
-            opt.default = {
-                theme: 'android-ics light', //皮肤样式
-                display: 'modal', //显示方式
-                mode: 'scroller', //日期选择模式
-                dateFormat: 'yyyy-mm-dd',
-                lang: 'zh',
-                showNow: true,
-                nowText: "今天",
-                startYear: currYear - 10, //开始年份
-                endYear: currYear + 10 //结束年份
-            };
 
-            $(".appDate").mobiscroll($.extend(opt['date'], opt['default']));
-        });
-    </script>
 @endsection
 @section('content')
     <h3 class="header-text">学校设置
@@ -76,4 +52,31 @@
             </div>
         </div><!-- ./smart-widget-inner -->
     </div><!-- ./smart-widget -->
+@endsection
+@section('footer')
+    <script src="/js/date/mobiscroll_002.js" type="text/javascript"></script>
+    <script src="/js/date/mobiscroll_004.js" type="text/javascript"></script>
+    <script src="/js/date/mobiscroll.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(function () {
+            var currYear = (new Date()).getFullYear();
+            var opt={};
+            opt.date = {preset : 'date'};
+            opt.datetime = {preset : 'datetime'};
+            opt.time = {preset : 'time'};
+            opt.default = {
+                theme: 'android-ics light', //皮肤样式
+                display: 'modal', //显示方式
+                mode: 'scroller', //日期选择模式
+                dateFormat: 'yyyy-mm-dd',
+                lang: 'zh',
+                showNow: true,
+                nowText: "今天",
+                startYear: currYear - 10, //开始年份
+                endYear: currYear + 10 //结束年份
+            };
+
+            $(".appDate").mobiscroll($.extend(opt['date'], opt['default']));
+        });
+    </script>
 @endsection
