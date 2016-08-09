13c0873bd89fb7bc6e6a08948340b802cd3b6e94
dyyhobby <dyy@dyy.name>
2016-08-09 16:10:16 +0800

    更新

--- resources/views/family/welcome1.blade.php
+++ resources/views/family/welcome1.blade.php
@@ -18,8 +18,8 @@
     </div>
     <div class="module">
         <ul>
-            <li onclick="notic();">
-                <a href="javascript:void(0);"><span><img src="/images/icon/icon_news.png" /></span><i></i>通知</a>
+            <li>
+                <a href="{{ url('/family/news/student',['id' => $student->id]) }}"><span><img src="/images/icon/icon_news.png" /></span><i></i>通知</a>
             </li>
             <li>
                 <a href="{{ url('/family/message') }}"><span><img src="/images/icon/leave_user.png" /></span><i></i>我的留言</a>
