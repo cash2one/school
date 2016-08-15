940640ddfb19911df04f64a2020ce8a88453a3b6
dyyhobby <dyy@dyy.name>
2016-08-15 17:50:51 +0800

    更新

--- resources/views/teacher/task/add.blade.php
+++ resources/views/teacher/task/add.blade.php
@@ -63,7 +63,7 @@
     </form>
 </div>
 <script>
-    wx.config(<?php echo $wechatJs->config(array('chooseImage','uploadImage','downloadImage'), true) ?>);
+    wx.config(<?php echo $wechatJs->config(array('chooseImage','uploadImage','downloadImage'), false) ?>);
 
     $(".up_img div img").bind('click',function(){
 
